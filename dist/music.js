b290f48bcbddc79df3ebaa0eff67517562741765
zmj2008 <zhanminjie@aliyun.com>
2019-08-19 20:19:07 +0800

    blog update

--- dist/music.js
+++ dist/music.js
@@ -6,7 +6,7 @@ const ap = new APlayer({
       {
         name: "红色",
         artist: '幼稚园杀手',
-        url: 'http://m10.music.126.net/20190819150314/d867f8b8838ab1c24ec44b31ba252f1e/ymusic/055f/0f0e/530f/25032b676f8d57b87eb9ded452e0ef51.mp3',
+        url: 'http://m10.music.126.net/20190819204104/2d0bdc9c603ac11a44071992b34ccf36/ymusic/055f/0f0e/530f/25032b676f8d57b87eb9ded452e0ef51.mp3',
         cover: 'http://p2.music.126.net/C14iCBb0gWujgZnCc9SwpA==/109951164298313032.jpg',
       }
     ]
