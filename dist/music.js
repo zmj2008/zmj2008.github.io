fb11a253c9560d3cb8bda9dd7c999be7a914b80a
13588245977 <zhanminjie@hotmail.com>
2019-08-22 06:46:59 +0800

    blog update

--- dist/music.js
+++ dist/music.js
@@ -17,16 +17,16 @@ const ap = new APlayer({
         cover: 'http://imge.kugou.com/stdmusic/150/20190605/20190605155235947489.jpg',
       },
 	  {
-        name: "画",
-        artist: 'G.E.M.邓紫棋',
-        url: 'http://fs.open.kugou.com/68c388c49ee08cea383b92e0ea01753d/5d5d2fb5/G072/M03/1B/04/iA0DAFc4Oq2ASH8UACk2YICuxZ0695.mp3',
+        name: "红色",
+        artist: '幼稚园杀手',
+        url: 'http://fs.open.kugou.com/c1fc91cdbec664a544399e18d30ac4fb/5d5dc7de/G164/M06/0A/11/RIcBAF1XrcSAOnIlADWD-68IWVA388.mp3',
         cover: 'http://singerimg.kugou.com/uploadpic/softhead/150/20190720/20190720220214641.jpg',
       },
 	  {
-        name: "起风了",
-        artist: '吴青峰',
-        url: 'http://fs.open.kugou.com/7a8151cc7804a2f2fb403ff8ddfcaf13/5d5d3247/G160/M06/14/09/4A0DAFzjihCAPfDzAEioCffXZ3U002.mp3',
-        cover: 'http://singerimg.kugou.com/uploadpic/softhead/150/20190708/20190708113533315.jpg',
+        name: "红色高跟鞋",
+        artist: '蔡健雅',
+        url: 'http://win.web.nf01.sycdn.kuwo.cn/f27a4fddcb974cfbe79ec04648c4533b/5d5dc93c/resource/n1/93/34/4174897214.mp3',
+        cover: 'http://img1.kuwo.cn/star/starheads/120/4/4baf6018723f680f891ea5383556e7e_0.jpg,
       }
     ]
 });
